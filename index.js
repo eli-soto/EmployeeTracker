07f470907087daf3bf6409bcb4ff8e21dcef8c9b
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-11-20 21:29:35 -0500

    creating function in js

--- index.js
+++ index.js
@@ -11,12 +11,31 @@ employee_tracker();
 let employee_tracker = function (){
 inquirer.prompt ([{
 type: 'list',
-name: 'promt',
+name: 'prompt',
 message: 'How will your proceed?',
-choices: []
+choices: ["view all employes", "add employees" , "exit"]
 
 
 
-}])
+
+}]) .then((answer)=> {
+    switch (answer.prompt) {
+       case "view all employes":
+            viewAllEmployess()
+            break; 
+            case "exit" :
+                 db.end()
+                 break; 
+    }
+})
 
 }
+
+function viewAllEmployess() {
+    db.query("select * from employess", (error, data) => {
+        if (error) throw error 
+        console.table(data) 
+        employee_tracker()
+    })
+}
+employee_tracker()
