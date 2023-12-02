0ad6a3e328df92571fbaa9535f67a7fb61cbbd34
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-12-02 16:41:04 -0500

    comments

--- tutor.js
+++ tutor.js
@@ -1,13 +1,13 @@
 
 
-const obj = {
-    firstName: "Dennis",
-    lastName: "Itua",
-    age: 18
-}
+// const obj = {
+//     firstName: "Eliceo",
+//     lastName: "Soto",
+//     age: 18
+// }
 
-const { firstName, lastName } = obj;
+// const { firstName, lastName } = obj;
 
-console.log(firstName)
-console.log(lastName)
-console.log(obj.age)
+// console.log(firstName)
+// console.log(lastName)
+// console.log(obj.age)
